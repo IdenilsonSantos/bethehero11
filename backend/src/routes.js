9500ebfa6eb161877e5bde81248c247c329444d0
idenilsonsantos <idenilsonoliveira.santos@gmail.com>
2020-03-27 12:01:14 -0300

    feat: backend controllers

---
+++ backend/src/routes.js
@@ -0,0 +1,19 @@
+const routes = require("express").Router();
+
+const OngController = require("./controllers/ongController");
+const IncidentController = require("./controllers/incidentController");
+const ProfileController = require("./controllers/profileController");
+const SessionController = require("./controllers/sessionController");
+
+routes.post("/sessions", SessionController.create);
+
+routes.get("/ongs", OngController.index);
+routes.post("/ongs", OngController.create);
+
+routes.get("/profiles", ProfileController.index);
+
+routes.get("/incidents", IncidentController.index);
+routes.post("/incidents", IncidentController.create);
+routes.delete("/incidents/:id", IncidentController.delete);
+
+module.exports = routes;
